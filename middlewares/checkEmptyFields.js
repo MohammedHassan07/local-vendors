4d7db06e85cbb52ab6095b493edb6e2afa55b8f1
MohammedHassan07 <mhassanscs260@gmail.com>
2024-09-22 11:46:07 +0530

    FEAT: Added one more check in  checkEmptyField

--- middlewares/checkEmptyFields.js
+++ middlewares/checkEmptyFields.js
@@ -2,23 +2,27 @@ function checkEmpty(req, res, next) {
 
     const data = req.body
 
+    if (Object.keys(data).length == 0) return res.status(400).json({
+
+        error: "Bad Request",
+        message: "Required values are empty."
+    })
+
     for (let key in data) {
 
         if (data.hasOwnProperty(key)) {
 
-            if (data[key] === null || data[key] === undefined || data[key] === '') {
+            if (data[key] === null || data[key] === undefined || data[key] === '')
 
                 return res.status(400).json({
 
                     error: "Bad Request",
                     message: "Required values are empty."
                 })
-                
-            }
         }
     }
 
     next()
-} 
+}
 
 module.exports = checkEmpty
